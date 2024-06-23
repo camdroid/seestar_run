3e1b865ebdd2ad6d1e80a79e3adde28e1995af44
Ben Guthro <ben@chaossearch.io>
2024-06-23 10:27:27 -0400

    Containerize seestar_run.py

---
+++ seestar_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+python ./seestar_run.py \
+    $SEESTAR_IP \
+    $SEESTAR_TARGET \
+    $SEESTAR_RA \
+    $SEESTAR_DEC \
+    $SEESTAR_LP_FILTER \
+    $SEESTAR_SESSION_TIME \
+    $SEESTAR_RA_PANEL_SZ \
+    $SEESTAR_DEC_PANEL_SZ \
+    $SEESTAR_RA_OFFSET_FACTOR \
+    $SEESTAR_DEC_OFFSET_FACTOR
